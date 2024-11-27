cb4f8d7c507d27136f8fcd9d3e26cb357180757f
YaffarOmarOZIL <119941955+YaffarOmarOZIL@users.noreply.github.com>
2024-11-27 16:01:16 -0400

    test de David_ osea yo gaaaaaaaaaaaaa
    Hola mundo php 2

---
+++ SQAFinal_David/test_cases/test_Gestion_de_Suministros/test_Despliegue_Menu.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+import time
+
+class TestRegistro:
+    def setup_method(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.get("http://127.0.0.1:8000/")
+        self.driver.find_element(By.XPATH, "//a[text() = 'Iniciar Sesion']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("admin@gmail.com")
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("cinnamonroll")
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        time.sleep(1)
+
+    def teardown_method(self):
+        self.driver.quit()
+        print(" Prueba Completa")
+
+    def test_Despliegue(self):
+        time.sleep(6)
+        self.driver.find_element(By.XPATH, "/html/body/div/aside/div/div[2]/ul/li[7]/a/span").click()
+        time.sleep(1)
+        actual = self.driver.find_element(By.XPATH, "/html/body/div/aside/div/div[2]/ul/li[7]/div/a[1]/span").text
+        esperado = "Proveedores"
+        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
