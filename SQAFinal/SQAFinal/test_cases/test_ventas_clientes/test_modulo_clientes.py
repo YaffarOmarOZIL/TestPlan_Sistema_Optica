b21318fbe479be394271bcdddf9f6696264a64d5
MiguelAguila <miguelaguila272004@gmail.com>
2024-12-03 02:37:52 -0400

    Modulo de clientes terminado

--- SQAFinal/SQAFinal/test_cases/test_ventas_clientes/test_modulo_clientes.py
+++ SQAFinal/SQAFinal/test_cases/test_ventas_clientes/test_modulo_clientes.py
@@ -24,66 +24,79 @@ class TestLogin:
         self.driver.quit()
         print(" Prueba visual completa")
 
-    def test_vefiricar_entrar_usuario(self):
-        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/user']").click()
+    def test_vefiricar_entrar_clientes(self):
+        self.driver.find_element(By.XPATH, "//*[@id='mobile-menu']/ul/li[5]/a").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/cliente']").click()
         time.sleep(2)
         actual = self.driver.find_element(By.XPATH, "//h1[@class='text-capitalize mb-0']").text
-        esperado = "Usuarios"
+        esperado = "Clientes"
         assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
 
-    def test_vefiricar_buscador_usuario(self):
-        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/user']").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("admin")
-        time.sleep(20)
-        actual = self.driver.find_element(By.XPATH, "//td[@class='dtr-control']//child::span").text
-        esperado = "admin"
-        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
-    
-    def test_vefiricar_agregar_usuario(self):
-        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/user']").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//i[@class='la la-plus']").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//input[@name='name']").send_keys("David Carpio")
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("dacar@gmail.com")
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("DavidCarpio56")
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//span[@data-value='save_and_back']").click()
-        time.sleep(10)
-        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("David Carpio")
-        time.sleep(20)
-        actual = self.driver.find_element(By.XPATH, "//td[@class='dtr-control']//child::span").text
-        esperado = "David Carpio"
-        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
-
-    def test_vefiricar_Editar_usuario(self):
-        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/user']").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//*[@id='crudTable']/tbody/tr/td[3]/a[2]/span").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//input[@name='name']").send_keys("David Carpios")
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//span[@data-value='save_and_back']").click()
-        time.sleep(10)
-        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("David Carpios")
-        time.sleep(20)
-        actual = self.driver.find_element(By.XPATH, "//td[@class='dtr-control']//child::span").text
-        esperado = "David Carpios"
-        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
-        
-    def test_vefiricar_Eliminar_usuario(self):
-        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/user']").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//*[@id='crudTable']/tbody/tr[1]/td[3]/a[3]/span").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//button[@class='swal-button swal-button--delete bg-danger']").click()
+    def test_vefiricar_buscador_clientes(self):
+        self.driver.find_element(By.XPATH, "//*[@id='mobile-menu']/ul/li[5]/a").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/cliente']").click()
         time.sleep(2)
         WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//input[@class='form-control']")))
-        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("David Carpios")
-        time.sleep(20)
-        actual = WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//td[@class='dataTables_empty']")))
+        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("Elvis")
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//td[@class='dtr-control']//child::span")))
+        actual = self.driver.find_element(By.XPATH, "//td[@class='dtr-control']//child::span").text
+        esperado = "Elvis"
+        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
+    
+    def test_verificar_agregar_cliente(self):
+        self.driver.find_element(By.XPATH, "//*[@id='mobile-menu']/ul/li[5]/a").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/cliente']").click()
+        time.sleep(2)
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//i[@class='la la-plus']")))
+        self.driver.find_element(By.XPATH, "//i[@class='la la-plus']").click()
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//input[@name='nombre']")))
+        self.driver.find_element(By.XPATH, "//input[@name='nombre']").send_keys("Jhoel")
+        self.driver.find_element(By.XPATH, "//input[@name='apellido']").send_keys("Neuenshwander")
+        self.driver.find_element(By.XPATH, "//input[@name='correo']").send_keys("joelneu@gmail.com")
+        self.driver.find_element(By.XPATH, "//input[@name='telefono']").send_keys("74576543")
+        self.driver.find_element(By.XPATH, "//span[@data-value='save_and_back']").click()
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//input[@class='form-control']")))
+        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("Jhoel")
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//td[@class='dtr-control']//child::span")))
+        actual = self.driver.find_element(By.XPATH, "//td[@class='dtr-control']//child::span").text
+        esperado = "Jhoel"
+        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
+
+    def test_verificar_Editar_cliente(self):
+        self.driver.find_element(By.XPATH, "//*[@id='mobile-menu']/ul/li[5]/a").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/cliente']").click()
+        time.sleep(2)
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='crudTable']/tbody/tr[1]/td[5]/a[2]/span")))
+        self.driver.find_element(By.XPATH, "//*[@id='crudTable']/tbody/tr[1]/td[5]/a[2]/span").click()
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//input[@name='nombre']")))
+        self.driver.find_element(By.XPATH, "//input[@name='nombre']").send_keys("J")
+        self.driver.find_element(By.XPATH, "//input[@name='apellido']").send_keys("N")
+        self.driver.find_element(By.XPATH, "//input[@name='correo']").send_keys("n")
+        self.driver.find_element(By.XPATH, "//span[@data-value='save_and_back']").click()
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//input[@class='form-control']")))
+        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("JJhoel")
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//td[@class='dtr-control']//child::span")))
+        actual = self.driver.find_element(By.XPATH, "//td[@class='dtr-control']//child::span").text
+        esperado = "JJhoel"
+        assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
+    
+    def test_verificar_Eliminar_cliente(self):
+        self.driver.find_element(By.XPATH, "//*[@id='mobile-menu']/ul/li[5]/a").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//a[@href ='http://127.0.0.1:8000/admin/cliente']").click()
+        time.sleep(2)
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='crudTable']/tbody/tr[1]/td[5]/a[3]/span")))
+        self.driver.find_element(By.XPATH, "//*[@id='crudTable']/tbody/tr[1]/td[5]/a[3]/span").click()
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//button[@class='swal-button swal-button--delete bg-danger']")))
+        self.driver.find_element(By.XPATH, "//button[@class='swal-button swal-button--delete bg-danger']").click()
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//input[@class='form-control']")))
+        self.driver.find_element(By.XPATH, "//input[@class='form-control']").send_keys("JJhoel")
+        WebDriverWait(self.driver, 50).until(EC.visibility_of_element_located((By.XPATH, "//td[@class='dataTables_empty']")))
+        actual = self.driver.find_element(By.XPATH, "//td[@class='dataTables_empty']").text
         esperado = "No se encontraron elementos"
         assert esperado in actual, f"ERROR: Actual es: {actual} y el Esperado: {esperado}"
+        
